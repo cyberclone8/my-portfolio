4eb8def3d7a1612379430aa8b195078ff92dfad2
cyberclone8 <lance.lopez0912@gmail.com>
2024-09-02 16:28:31 +0800

    Update config.py

--- python_api/config.py
+++ python_api/config.py
@@ -3,28 +3,24 @@ import enum
 
 
 # MSSQL Connector
-SERVER = '10.147.18.59'
-DATABASE = 'intellizoo'
-USERNAME = 'lance2'
-PASSWORD = 'wEh1zqsZZ0kSNEp1'
-# cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
-# # cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database, Trusted_connection='Yes')
-# cnxn.autocommit = True
-# cursor = cnxn.cursor()
+SERVER = ''
+DATABASE = ''
+USERNAME = ''
+PASSWORD = ''
 
 DUMP_DIR = './dump'
 
 # MongoDB Connection String
-CONNECTION_STRING = "mongodb://izoo:rSTaCmGRS573amuq@54.38.232.79:27017/?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
+CONNECTION_STRING = ""
 
 # Azure creds
-AZURE_STORAGE_CONNECTION_STRING="DefaultEndpointsProtocol=https;AccountName=izooblobuat;AccountKey=quV4RJ0BB0ffel5mqZ28Xjqnuy9rwmrmJRfZ0SXgmPlO4y0upuVWOQcbsDqX9ZmF4YC1IQ1jYxlp9FTvat/+og==;EndpointSuffix=core.windows.net"
+AZURE_STORAGE_CONNECTION_STRING=""
 AZURE_FILES_DIR = './uploads'
 AZURE_CONTAINER_NAME = 'rootcontainer'
 AZURE_REMOTE_PATH_TWITTER = 'UploadedDoc/CentralizedStandAlone/Twitter/'
 AZURE_REMOTE_PATH_FACEBOOK = 'UploadedDoc/CentralizedStandAlone/Facebook/'
-SECRET_KEY = 'hRZYpJz0YBxuAwvPQaHfue5Oja'
-CRYPTO_KEY = b'OivegknqMox4wXHHTh2m8DOY7pAru-q8dmnJggG3wsU='
+SECRET_KEY = ''
+CRYPTO_KEY = ''
 
 
 class StandAloneModule(enum.Enum):
